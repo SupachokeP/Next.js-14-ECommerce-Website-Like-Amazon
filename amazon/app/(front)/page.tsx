2d944de2ce0c978bac819eb411c7789aed0c4819
Supachoke Boonsirivit <101698535+SupachokeP@users.noreply.github.com>
2024-03-19 22:04:34 +0700

    day 2 : เซฟย้อนหลัง

--- amazon/app/(front)/page.tsx
+++ amazon/app/(front)/page.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import ProductItem from "@/components/products/ProductItem";
+import data from "@/lib/data";
 
 export default function Home() {
-  return <h2>Lastest Product</h2>;
+  return (<>
+    <h2>Lastest Product</h2>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols3 lg:grid-cols-4">
+    {data.products.map((product)=>(
+      <ProductItem key ={product.slug}product={product} />
+    ))}
+    </div>
+    </>)
+  
+  
 }
